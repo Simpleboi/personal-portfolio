58f6c4c8f042bde8381abb1cd5b1823b68524926
Biggie Cheese <Jv3studios@gmail.com>
2025-01-05 18:11:38 -0600

    Added a search blog component

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -8,9 +8,39 @@ export const BlogBanner = () => {
       <h1>
         Thoughts & <span>&lt;Insights/&gt;</span>
       </h1>
-      <h2>Sharing my <span>&lt;Journey/&gt;</span> through web development, Technology, and life</h2>
-      <div><i className='bx bx-chevron-down'></i></div>
+      <h2>
+        Sharing my <span>&lt;Journey/&gt;</span> through web development,
+        Technology, and life
+      </h2>
+      <div>
+        <i className="bx bx-chevron-down"></i>
+      </div>
     </div>
+  );
+};
+
+const Filters = [
+  "Web Development",
+  "React",
+  "TypeScript",
+  "AI",
+  "Technology",
+  "Future Trends",
+  "Programming",
+];
+
+export const BlogFilter = () => {
+  return (
+    <section className="blog-filter-container">
+      <div className="filter-top">
+        <input type="text" placeholder="Search &lt;blog/&gt; posts..." />
+      </div>
+      <div className="filter-bottom">
+        {Filters.map((element) => {
+          return <button>{element}</button>;
+        })}
+      </div>
+    </section>
   );
 };
 
@@ -19,6 +49,7 @@ export const Blog = () => {
     <section id="blog">
       <Nav />
       <BlogBanner />
+      <BlogFilter />
     </section>
   );
 };
