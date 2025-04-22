05997dde9de5464b3672ca0ae1cf1a3bcb458fcc
simpleboi <jv3studios@gmail.com>
2025-04-22 04:26:16 +0000

    configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,25 @@ import { Skills } from "./pages/skills";
 import { About } from "./pages/about";
 import { Contact } from "./pages/contact";
 import { Blog } from "./pages/Blog";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { BlogPostDetails } from "./pages/Blog";
 import { Portfolio } from "./pages/Portfolio";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Blog" element={<Blog />}></Route>
-        <Route path="/blog/:id" element={<BlogPostDetails />} />
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/skills" element={<Skills />}></Route>
-        <Route path="/portfolio" element={<Portfolio />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Blog" element={<Blog />} />
+      <Route path="/blog/:id" element={<BlogPostDetails />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      {/* Add this before any catchall route */}
+      {import.meta.env.VITE_TEMPO && (
+        <Route path="/tempobook/*" element={null} />
+      )}
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+// Import the dev tools and initialize them
+import { TempoDevtools } from "tempo-devtools";
+import { BrowserRouter } from "react-router-dom";
+import { TempoRoutes } from "./TempoRoutes";
 
-createRoot(document.getElementById('root')!).render(
+TempoDevtools.init();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <TempoRoutes />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
