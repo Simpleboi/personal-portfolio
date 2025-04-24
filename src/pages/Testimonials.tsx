5a0823432cb1dd98dc4604b4ddc7b168a111a628
Biggie Cheese <Jv3studios@gmail.com>
2025-04-24 12:06:56 -0500

    fixed testimonial section

--- src/pages/Testimonials.tsx
+++ src/pages/Testimonials.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { TestimonialCard } from "../components/TestimonialCard";
 import "../styles/Testimonials.scss";
 import { Nav } from "../components/nav";
+import sjc from "/assets/images/sjc-logo.jpg"
 
 export const Testimonials: React.FC = () => {
-  // Sample testimonial data - in a real app, this might come from an API or CMS
   const testimonials = [
     {
       id: 1,
       quote:
-        "This is temporary Text",
-      name: "Name",
-      title: "Job Title",
-      image: "../../assets/images/testimonials/sarah.jpg",
+        "Nathaniel and I worked together on our college's Computer Science Club website. While I handled the design and color choices, he took on the technical side, managing the scripting and content. As president of the club, he helped introduce topics that went beyond what we learned in class, like navigating the job market or programming practices that aren't usually covered in coursework. His passion for learning and diving into new challenges really stood out to me; it's a mindset I believe every CS student should have. In a field where experience outside the classroom matters just as much as grades, he's one of the few who genuinely put in that extra effort.",
+      name: "Derrick Martinez ",
+      title: "Professor of Computer Science | San Jacinto College",
+      image: sjc,
     },
     {
       id: 2,
@@ -21,14 +21,6 @@ export const Testimonials: React.FC = () => {
       name: "Name",
       title: "Job Title",
       image: "../../assets/images/testimonials/michael.jpg",
-    },
-    {
-      id: 3,
-      quote:
-        "Temporary Text",
-      name: "Name",
-      title: "Computer Science Professor, Tech University",
-      image: "../../assets/images/testimonials/emily.jpg",
     },
   ];
 
