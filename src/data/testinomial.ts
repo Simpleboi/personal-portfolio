bcb67fe15817cbe0b8df68a520af80a6eed5430f
Biggie Cheese <Jv3studios@gmail.com>
2025-04-24 12:33:47 -0500

    changed the blog post styles

---
+++ src/data/testinomial.ts
@@ -0,0 +1,19 @@
+import sjc from "/assets/images/sjc-logo.jpg"
+
+export const testimonials = [
+  {
+    id: 1,
+    quote:
+      "Nathaniel and I worked together on our college's Computer Science Club website. While I handled the design and color choices, he took on the technical side, managing the scripting and content. As president of the club, he helped introduce topics that went beyond what we learned in class, like navigating the job market or programming practices that aren't usually covered in coursework. His passion for learning and diving into new challenges really stood out to me; it's a mindset I believe every CS student should have. In a field where experience outside the classroom matters just as much as grades, he's one of the few who genuinely put in that extra effort.",
+    name: "Derrick Martinez ",
+    title: "Professor of Computer Science | San Jacinto College",
+    image: sjc,
+  },
+  {
+    id: 2,
+    quote: "This is a Placeholder Testimonal",
+    name: "Name",
+    title: "Job Title",
+    image: "/assets/images/testimonials/michael.jpg",
+  },
+];
