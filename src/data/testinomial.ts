7df1e602621d5f1f4206490bb6ed2a100efe1baa
Biggie Cheese <Jv3studios@gmail.com>
2025-04-24 12:42:33 -0500

    fixed blog post changes

--- src/data/testinomial.ts
+++ src/data/testinomial.ts
@@ -6,7 +6,7 @@ export const testimonials = [
     quote:
       "Nathaniel and I worked together on our college's Computer Science Club website. While I handled the design and color choices, he took on the technical side, managing the scripting and content. As president of the club, he helped introduce topics that went beyond what we learned in class, like navigating the job market or programming practices that aren't usually covered in coursework. His passion for learning and diving into new challenges really stood out to me; it's a mindset I believe every CS student should have. In a field where experience outside the classroom matters just as much as grades, he's one of the few who genuinely put in that extra effort.",
     name: "Derrick Martinez ",
-    title: "Professor of Computer Science | San Jacinto College",
+    title: "Professor of Computer Information Technology | San Jacinto College",
     image: sjc,
   },
   {
