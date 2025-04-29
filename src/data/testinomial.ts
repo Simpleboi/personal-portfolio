d405361758ce15d2ed749d4b66360bb69f8c452b
Biggie Cheese <Jv3studios@gmail.com>
2025-04-28 22:31:04 -0500

    added a new testimonial

--- src/data/testinomial.ts
+++ src/data/testinomial.ts
@@ -13,7 +13,14 @@ export const testimonials = [
     id: 2,
     quote: "Nate and I worked together in a local STEM challenge. During the coding portion, he focused on the most difficult of the prompts while the rest of us either implemented code or wrote pseudocode for the many simple prompts. He was also the President in our Computer Science club. He was always encouraging us to learn, and excited to give the week's presentation. Nate shows his passion for programing with his many side projects and truly pushes himself to be the best. He will be an asset to any team.",
     name: "Josh Dykstra",
-    title: "Treasurer of the Computer Science Club | San Jacinto College",
+    title: "Treasurer, Computer Science Club | San Jacinto College",
     image: sjc,
   },
+  {
+    id: 3,
+    quote: "I first met Nate during our in-person Programming Fundamentals class, and it was immediately clear he was a special case. We shared common interests in cybersecurity and development, but what truly stood out about Nate was his unwavering determination to learn and master the material. He consistently put in the effort to truly understand what we were studying, a quality that's rare to find. His drive and dedication set him apart from the rest.",
+    name: "Emmanuel Attagba",
+    title: "Secretary, Computer Science Club | San Jacinto College",
+    image: sjc,
+  }
 ];
