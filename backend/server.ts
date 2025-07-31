b167cdcdc69d335c8d25a75c01077be180e638aa
Biggie Cheese <Jv3studios@gmail.com>
2025-07-31 17:22:48 -0500

    fixed env issue

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,6 @@
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://nate:KQfrFYhXD96jJKkI@cluster0.6ukbar6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "something";
 
 // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 // const client = new MongoClient(uri, {
